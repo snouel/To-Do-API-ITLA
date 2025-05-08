58e02c6c9bdaf5ea342c3bb3c85e5bc50a709338
snouel <104464911+snouel@users.noreply.github.com>
2025-05-07 21:26:12 -0400

    Project Stage 2

---
+++ To-Do API/Helpers/TaskDelegates.cs
@@ -0,0 +1,18 @@
+﻿using To_Do_API.Models;
+
+namespace To_Do_API.Helpers
+{
+    public class TaskDelegates
+    {
+        public delegate bool ValidateTask(TodoTask<string> task);
+
+        public static ValidateTask validate = task => !string.IsNullOrWhiteSpace(task.Description) && task.DueDate > DateTime.UtcNow;
+
+        public static Action<TodoTask<string>> NotifyCreation = task =>
+        {
+            Console.WriteLine($"Task Created: {task.Description} with ID: {task.Id}");
+        };
+
+        public static Func<TodoTask<string>, int> CalculateDayLeft = task => (task.DueDate - DateTime.Now).Days;
+    }
+}
